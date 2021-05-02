f20028e92b6162e556b7e9beaca85e9130cd4194
Sam Pilehvarian <samp.codes@gmail.com>
2021-05-02 11:07:26 -0400

    Add comments + clean up the code

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 // Components
-import AlbumContainer from './components/Album';
 import Album from './components/Album/Album';
 
 import AlbumsContainer from './components/Albums';
@@ -15,39 +14,52 @@ import { BrowserRouter } from 'react-router-dom';
 import { albums, photos } from './test/mocks';
 
 test('renders Albums', () => {
+	// First, render Albums container wrapping it with the Albums Provider (so it can read the albums)
 	render(
 		<AlbumsProvider>
 			<AlbumsContainer />
 		</AlbumsProvider>
 	);
 
+	// Search for Photo Galleries text in the page
 	const titleElement = screen.getByText(/Photo Galleries:/i);
+	// Photo Galleries text is expected to show up to tell if AlbumsContainer has been rendered
 	expect(titleElement).toBeInTheDocument();
 });
 
-test('There are 10 albums', () => {
+test(`There are ${albums.length} albums`, () => {
+	// Render Albums with mock data and saves the component into the container variable
 	const { container } = render(
 		<BrowserRouter>
 			<Albums albums={albums} />
 		</BrowserRouter>
 	);
 
+	// Get all the <li> inside <Albums />
 	const lis = container.querySelectorAll('li');
-	expect(lis.length).toBe(10);
+
+	// There should be as many <li> as albums in albums
+	expect(lis.length).toBe(albums.length);
 });
 
-test('There are 50 photos', () => {
+test(`There are ${photos[1].length} photos`, () => {
 	const { container } = render(<Album album={photos[1]} search={''} />);
 
 	const img = container.querySelectorAll('img');
-	expect(img.length).toBe(50);
+
+	// There should be as many <img> elements as photos in photos[1]
+	expect(img.length).toBe(photos[1].length);
 });
 
 test('Filters photos', () => {
+	// Renders album with photos and a search equal to "accu"
 	const { container } = render(<Album album={photos[1]} search={'accu'} />);
 
+	// Given the photos in photos[1] and that search, we expect to get 2 elements/matches
 	const img = container.querySelectorAll('img');
 	expect(img.length).toBe(2);
+
+	// There is a photo that matches the search and has this search; should find it
 	expect(
 		screen.getByText('ea aliquid et amet sequi nemo')
 	).toBeInTheDocument();
